c2696759fd542e1e4a22afc53176f1b10a2ef7fc
Arnon.r <71333954+richmanz08@users.noreply.github.com>
2025-11-25 09:32:25 +0700

    let started

---
+++ src/main/java/com/insidergame/insider_api/InsiderApiApplication.java
@@ -0,0 +1,13 @@
+package com.insidergame.insider_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsiderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsiderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insidergame/insider_api/InsiderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insidergame.insider_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsiderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
